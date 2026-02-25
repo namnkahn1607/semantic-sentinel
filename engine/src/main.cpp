5947c88f53a9200ec0a1f73383f1c8b5e9aed262
namnkahn1607 <nlnkandthebois@gmail.com>
2026-02-25 18:09:32 +0700

    Initial Semantic Service implementation

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,3 +1,23 @@
+#include <grpcpp/grpcpp.h>
+
+#include "proto/sentinel.grpc.pb.h"
+
+class SemanticServiceImpl final : public proto::SemanticService::Service {
+public:
+    grpc::Status CheckCache(grpc::ServerContext* context,
+                            const proto::CheckCacheRequest* request,
+                            proto::CheckCacheResponse* response) override {
+        if (request->prompt_text() == "hello") {
+            response->set_is_hit(true);
+            response->set_cached_payload("world");
+        } else {
+            response->set_is_hit(false);
+        }
+
+        return grpc::Status::OK;
+    };
+};
+
 int main() {
     return 0;
 }
