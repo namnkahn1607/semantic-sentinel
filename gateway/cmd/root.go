20d399e8207742a74f2479c01c134fdc2193c16b
namnkahn1607 <nlnkandthebois@gmail.com>
2026-04-30 01:04:31 +0700

    Initiate CLI entry point

--- gateway/cmd/root.go
+++ gateway/cmd/root.go
@@ -1,12 +1,25 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "strix",
 	Short: "Strix - Semantic Cache Proxy CLI",
 	Long: `Strix is a high-performance semantic cache gateway.
 	Use 'strix init' to set up your environment, then 'strix serve' to start.`,
+}
+
+// Execute is the single entry point called by main.go.
+func Execute() {
+	if execErr := rootCmd.Execute(); execErr != nil {
+		fmt.Printf("[strix] Entry point error: %v", execErr)
+		os.Exit(1)
+	}
 }
 
 func init() {
--- gateway/main.go
+++ gateway/main.go
@@ -1,3 +1,7 @@
 package main
 
-func main() {}
+import "gateway/cmd"
+
+func main() {
+	cmd.Execute()
+}
