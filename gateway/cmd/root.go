86eb0c02f2dd14817f980577dd1e0f71ac58cea4
namnkahn1607 <nlnkandthebois@gmail.com>
2026-04-30 00:16:07 +0700

    'root' CLI command

---
+++ gateway/cmd/root.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "strix",
+	Short: "Strix - Semantic Cache Proxy CLI",
+	Long: `Strix is a high-performance semantic cache gateway.
+	Use 'strix init' to set up your environment, then 'strix serve' to start.`,
+}
+
+func init() {}
