06792da2a0a11ea3537f8b08d62c121e0e45250b
namnkahn1607 <nlnkandthebois@gmail.com>
2026-05-03 23:15:26 +0700

    Worker Pool WRITE uncached payload in the background

---
+++ gateway/internal/proxy/worker_pool.go
@@ -0,0 +1,65 @@
+package proxy
+
+import (
+	"context"
+	pb "gateway/pb/proto"
+	"log"
+	"time"
+)
+
+const (
+	numWorkers  = 4
+	jobQueueCap = 5000
+	jobTimeout  = 1 * time.Second
+)
+
+type Job struct {
+	NodeID  int32
+	Payload []byte
+}
+
+type WorkerPool struct {
+	queue chan Job
+}
+
+func NewWorkerPool(stub pb.SemanticServiceClient) *WorkerPool {
+	wp := &WorkerPool{
+		queue: make(chan Job, jobQueueCap),
+	}
+
+	for range numWorkers {
+		go wp.runWorker(stub)
+	}
+
+	return wp
+}
+
+func (wp *WorkerPool) TryEnqueue(nodeID int32, payload []byte) {
+	select {
+	case wp.queue <- Job{NodeID: nodeID, Payload: payload}:
+	default:
+	}
+}
+
+func (wp *WorkerPool) Stop() {
+	close(wp.queue)
+}
+
+func (wp *WorkerPool) runWorker(stub pb.SemanticServiceClient) {
+	for job := range wp.queue {
+		ctx, cancel := context.WithTimeout(context.Background(), jobTimeout)
+
+		_, rpcErr := stub.SetCache(ctx, &pb.SetCacheRequest{
+			NodeId:          job.NodeID,
+			UncachedPayload: job.Payload,
+		})
+
+		cancel()
+
+		if rpcErr != nil {
+			log.Printf("[Worker Pool] RPC Write failed for nodeID = %d: %v\n",
+				job.NodeID, rpcErr,
+			)
+		}
+	}
+}
